1a135fcf6e4dbac34f8dbfca308e59ad3c3ca978
Md Mazharul Islam <52231250+mazhaurl@users.noreply.github.com>
2023-08-24 18:05:20 +0600

    Add files via upload

---
+++ menu.py
@@ -0,0 +1,11 @@
+#INSTALL------------------------------------------------------------------------
+
+
+
+import md_backdrop_v3
+
+
+
+
+
+utilitiesMenu.addCommand('md_backdrop_v3','md_backdrop_v3.md_backdrop_go()','shift+B', shortcutContext=2 )
